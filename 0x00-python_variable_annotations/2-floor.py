f7eecc1bb83a8084cd35489e15511bf5ad5b6739
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-11 23:35:30 +0100

    feat: Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Attributes:
+        n(float)
+    Return:
+        the floor of n(int)
+    """
+    return math.floor(n)
