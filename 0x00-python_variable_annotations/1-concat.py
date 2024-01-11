ed6a29bae75403fd9e0a30adfc8a2f11592ca486
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-11 23:28:09 +0100

    feat: Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Attributes:
+        str1(string)
+        str2(string)
+    Return:
+        string
+    """
+    return str1+str2
