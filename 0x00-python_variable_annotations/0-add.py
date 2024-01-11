c65d14d412339af5149d6b52bb63b0fe528eaaf5
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-11 23:22:55 +0100

    feat: Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Attributes:
+        a(float)
+        b(float)
+    Return:
+        float
+    """
+    return a + b
