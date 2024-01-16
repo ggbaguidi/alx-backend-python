d24bf86ceca6d11067af9d45f1f20aa843307ef0
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-16 11:04:39 +0100

    feat: add Async Generator file

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    Attributes:
+        no arguments
+    Return:
+        The coroutine will loop 10 times, each time asynchronously
+        wait 1 second, then yield a random number between 0 and 10.
+    """
+    for _ in range(10):
+        yield random.random() * 10
+        await asyncio.sleep(1)
